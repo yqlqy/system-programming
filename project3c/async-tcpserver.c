2be5043bf886c015fa085fdfec193968765a9fdd
Coco <liucoco1987@gmail.com>
2021-11-23 06:33:27 +0000

    Reorg projects folder

---
+++ project3c/async-tcpserver.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+
+/* This is a reference socket server implementation that prints out the messages
+ * received from clients. */
+
+#define MAX_PENDING 100
+#define MAX_LINE 10
+#define ARRAY_SIZE 1024
+
+void *connectToClient(void *arg);
+
+int main(int argc, char *argv[]) {
+  // char* host_addr = argv[1];
+  // int port = atoi(argv[2]);
+  /* hard-coded ip address for project3a*/
+  char* host_addr = "127.0.0.1";
+  int port = atoi(argv[1]);
+
+  /*setup passive open*/
+  int s;
+  if((s = socket(PF_INET, SOCK_STREAM, 0)) <0) {
+    perror("simplex-talk: socket");
+    exit(1);
+  }
+
+  /* Config the server address */
+  struct sockaddr_in sin;
+  sin.sin_family = AF_INET; 
+  sin.sin_addr.s_addr = inet_addr(host_addr);
+  sin.sin_port = htons(port);
+  // Set all bits of the padding field to 0
+  memset(sin.sin_zero, '\0', sizeof(sin.sin_zero));
+
+  /* Bind the socket to the address */
+  if((bind(s, (struct sockaddr*)&sin, sizeof(sin)))<0) {
+    perror("simplex-talk: bind");
+    exit(1);
+  }
+  // connections can be pending if many concurrent client requests
+  listen(s, MAX_PENDING);  
+ 
+  /* wait for connection, then receive and print text */
+  
+
+  pthread_t tids[MAX_PENDING];
+  int sockDfs[MAX_PENDING];
+  int i = 0;
+  int id[MAX_PENDING];
+
+  // for (i=0; i<MAX_PENDING; i++) id[i] = i;
+
+  // srand(0);  
+
+
+  while(1) {
+    int new_s;
+    socklen_t len = sizeof(sin);
+
+    if((new_s = accept(s, (struct sockaddr *)&sin, &len)) <0){
+      perror("simplex-talk: accept");
+      exit(1);
+    }
+    sockDfs[i] = new_s;
+    if(pthread_create(&tids[i++], NULL, connectToClient, &new_s) != 0 )
+           printf("Failed to create thread\n");
+    //printf("Created thread %d\n", i);
+
+    if(i >= MAX_PENDING)
+    {
+      i = 0;
+      while(i < MAX_PENDING)
+      {
+        if (pthread_join(tids[i],NULL) == 0) {
+          close(sockDfs[i]);
+        }
+        i++;
+        //printf("Checking thread %d termination status\n", i);
+      }
+      i = 0;
+    }    
+  }
+
+  return 0;
+}
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+void *connectToClient(void *arg) {
+  int seq = 0;
+  char buf[ARRAY_SIZE];
+  char number[ARRAY_SIZE];
+  int thread_s = *((int *)arg);
+  int len;
+  //printf("Current socket fd: %d\n", thread_s);
+  pthread_mutex_lock(&lock);
+  while(len = recv(thread_s, buf, sizeof(buf), 0)){
+    //buf[len] = '\0';
+    if (seq > 0) {
+      //char expectedMsg[ARRAY_SIZE] = "HELLO ";
+      //memset(number, 0, strlen(number));
+      //sprintf(number, "%d", seq+1);
+      //strcat(expectedMsg, number);
+      //expectedMsg[strlen(expectedMsg)] = 0;
+      char expectedMsg[ARRAY_SIZE];
+      snprintf(expectedMsg, ARRAY_SIZE, "HELLO %d", seq+1);
+      //printf("Expected %s; received %s", expectedMsg, buf);
+      if (strcmp(expectedMsg, buf) == 0) {
+        fputs(buf, stdout);
+        fputs("\n", stdout);
+        fflush(stdout);
+        //fflush(stdout);
+      } else {
+        perror("ERROR");
+      }
+      continue;
+    }
+    fputs(buf, stdout);
+    fputs("\n", stdout);
+    fflush(stdout);
+    char *pt = strtok(buf, " ");
+    char ** res = NULL;
+    int idx =0;
+    while (pt) {
+      res = (char **)realloc(res, sizeof(char*) * strlen(buf));
+      if (res == NULL) exit(-1);
+      res[idx++] = pt;
+      pt = strtok(NULL, " ");
+    }
+    seq = atoi(res[1]);
+    seq++;
+    memset(number, 0, sizeof(number));
+    sprintf(number, " %d", seq);
+    strcat(res[0], number);
+    send(thread_s, res[0], strlen(buf), 0);
+    memset(buf, 0, sizeof(buf));
+  }
+  pthread_mutex_unlock(&lock);
+  //close(thread_s);
+}
---
+++ project3c/tcpclient.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+
+#define MAX_LINE 20
+#define ARRAY_SIZE 1024
+//int isMatch(char assert[], char msg[]);
+
+int main (int argc, char *argv[]) {
+  char* host_addr = argv[1];
+  int port = atoi(argv[2]);
+
+  /* Open a socket */
+  int s;
+  if((s = socket(PF_INET, SOCK_STREAM, 0)) <0){
+    perror("simplex-talk: socket");
+    exit(1);
+  }
+
+  /* Config the server address */
+  struct sockaddr_in sin;
+  sin.sin_family = AF_INET; 
+  sin.sin_addr.s_addr = inet_addr(host_addr);
+  sin.sin_port = htons(port);
+  // Set all bits of the padding field to 0
+  memset(sin.sin_zero, '\0', sizeof(sin.sin_zero));
+
+  /* Connect to the server */
+  if(connect(s, (struct sockaddr *)&sin, sizeof(sin))<0){
+    perror("simplex-talk: connect");
+    close(s);
+    exit(1);
+  }
+
+  /*main loop: get and send lines of text */
+  // char buf[MAX_LINE];
+  int count = atoi(argv[3]);
+  int threeWayHandShaking = 0;
+  while (1){//((fgets(buf, sizeof(buf),stdin)) {
+    //char sendInfo[] = "HELLO ";
+    //char number[10];
+    //memset(number, 0, strlen(number)+1);
+    //snprintf(number, 10, "%d", count);
+    //strcat(sendInfo, argv[3]);
+    char sendInfo[ARRAY_SIZE];
+    snprintf(sendInfo, ARRAY_SIZE, "HELLO %d", count);
+    int len = strlen(sendInfo)+1;
+    //printf("Got params: %d; Client send ori msg; %s\n", count, sendInfo);
+    send(s, sendInfo, len, 0);
+    threeWayHandShaking++;
+    count++;
+    char buf[ARRAY_SIZE];
+    memset(sendInfo, 0, strlen(sendInfo));
+    //memset(number, 0, strlen(number));
+    
+    if (threeWayHandShaking == 2) {
+      close(s);
+      //printf("Closing socket and breaking...");
+      break;
+    }
+
+    //char expectedMsg[ARRAY_SIZE] = "HELLO ";
+    //sprintf(number, "%d", count);
+    //strcat(expectedMsg, number);
+    //expectedMsg[strlen(expectedMsg)] = 0;
+    char expectedMsg[ARRAY_SIZE];
+    snprintf(expectedMsg, ARRAY_SIZE, "HELLO %d", count);
+    int msgLen;
+    msgLen = recv(s, buf, sizeof(buf), 0);
+    buf[msgLen] = '\0';
+    //printf("Expected msg: %s\n", expectedMsg);
+    //printf("Buf msg: %s\n", buf); 
+    //printf("Comparison: %d\n", strcmp(expectedMsg, buf));
+    if (strcmp(expectedMsg, buf) == 0) {
+      fputs(buf, stdout);
+      fputs("\n", stdout);
+      fflush(stdout);
+      count++;
+    } else {
+      perror("ERROR");
+    }
+
+    
+    //sleep(3);
+  }
+
+  return 0;
+}
+
+//int isMatch(char assert[], char msg[]) {
+//  int len;
+//  len = strlen(msg);
+//  int i;
+//  for (i = 0; i < len; i++) {
+//    if (msg[i] == '\n' || msg[i] == 0) {
+//      return 1;
+//    } else if (msg[i] != assert[i]) {
+//      return 0;
+//    }
+//  }
+//  return 1;
+//}
