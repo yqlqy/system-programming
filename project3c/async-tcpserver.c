36c0cd49acb7da1473f448e346cc66d422e3963f
Coco <liucoco1987@gmail.com>
2021-11-24 17:43:28 -0800

    Applied non-blocking way by fcntl

--- project3c/async-tcpserver.c
+++ project3c/async-tcpserver.c
@@ -10,11 +10,12 @@
 #include <errno.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <fcntl.h>
 
 /* This is a reference socket server implementation that prints out the messages
  * received from clients. */
 
-#define MAX_PENDING 100
+#define MAX_PENDING 1000
 #define MAX_LINE 10
 #define ARRAY_SIZE 1024
 
@@ -62,65 +63,43 @@ int main(int argc, char *argv[])
   }
   // connections can be pending if many concurrent client requests
   listen(s, MAX_PENDING);
-  fd_set active_fd_set, read_fd_set;
-  FD_ZERO(&active_fd_set);
-  FD_SET(s, &active_fd_set);
-  /* wait for connection, then receive and print text */
+  fd_set fd_set_for_all;
+  fd_set fd_set_in_use;
+  FD_ZERO(&fd_set_for_all);
+  FD_SET(s, &fd_set_for_all);
 
-  // pthread_t tids[MAX_PENDING];
-  // int sockDfs[MAX_PENDING];
-  // int i = 0;
-  // int id[MAX_PENDING];
-
-  // for (i=0; i<MAX_PENDING; i++) id[i] = i;
-
-  // srand(0);
-
-  Client reads[100];
+  Client reads[MAX_PENDING];
   while (1)
   {
     socklen_t len = sizeof(sin);
 
-    read_fd_set = active_fd_set;
-    select(FD_SETSIZE, &read_fd_set, NULL, NULL, NULL);
+    fd_set_in_use = fd_set_for_all;
+    select(FD_SETSIZE, &fd_set_in_use, NULL, NULL, NULL);
 
     int fd;
-    for (fd = 0; fd < FD_SETSIZE; fd++) {
-      if (FD_ISSET(fd, &read_fd_set)) {
-        if (fd == s) {
+    for (fd = 0; fd < FD_SETSIZE; fd++)
+    {
+      if (FD_ISSET(fd, &fd_set_in_use))
+      {
+        if (fd == s)
+        {
           int new_s;
           new_s = accept(s, (struct sockaddr *)&sin, &len);
-          FD_SET(new_s, &active_fd_set);
-        } else {
+          // fcntl(new_s, F_SETFL, fcntl(new_s, F_GETFL, 0) | O_NONBLOCK);
+          int flags = fcntl(fd, F_GETFL, 0);
+          fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+          FD_SET(new_s, &fd_set_for_all);
+        }
+        else
+        {
           handle_first_shake(fd);
+          // sleep(0.2);
           handle_second_shake(fd);
           close(fd);
-          FD_CLR(fd, &active_fd_set);
+          FD_CLR(fd, &fd_set_for_all);
         }
       }
     }
-
-    // if ((new_s = accept(s, (struct sockaddr *)&sin, &len)) < 0)
-    // {
-    //   perror("simplex-talk: accept");
-    //   exit(1);
-    // }
-    // sockDfs[i] = new_s;
-    // if(pthread_create(&tids[i++], NULL, connectToClient, &new_s) != 0 )
-    //        printf("Failed to create thread\n");
-
-    // if(i >= MAX_PENDING)
-    // {
-    //   i = 0;
-    //   while(i < MAX_PENDING)
-    //   {
-    //     if (pthread_join(tids[i],NULL) == 0) {
-    //       close(sockDfs[i]);
-    //     }
-    //     i++;
-    //   }
-    //   i = 0;
-    // }
   }
 
   return 0;
@@ -184,47 +163,3 @@ void handle_second_shake(int fd)
     perror("ERROR");
   }
 }
-
-// // pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-// void *connectToClient(void *arg) {
-//   int seq = 0;
-//   char buf[ARRAY_SIZE];
-//   char number[ARRAY_SIZE];
-//   int thread_s = *((int *)arg);
-//   int len;
-//   //  pthread_mutex_lock(&lock);
-//   while(len = recv(thread_s, buf, sizeof(buf), 0)){
-//      if (seq > 0) {
-//       char expectedMsg[ARRAY_SIZE];
-//       snprintf(expectedMsg, ARRAY_SIZE, "HELLO %d", seq+1);
-//       if (strcmp(expectedMsg, buf) == 0) {
-//         fputs(buf, stdout);
-//         fputs("\n", stdout);
-//         fflush(stdout);
-//       } else {
-//         perror("ERROR");
-//       }
-//       continue;
-//     }
-//     fputs(buf, stdout);
-//     fputs("\n", stdout);
-//     fflush(stdout);
-//     char *pt = strtok(buf, " ");
-//     char ** res = NULL;
-//     int idx =0;
-//     while (pt) {
-//       res = (char **)realloc(res, sizeof(char*) * strlen(buf));
-//       if (res == NULL) exit(-1);
-//       res[idx++] = pt;
-//       pt = strtok(NULL, " ");
-//     }
-//     seq = atoi(res[1]);
-//     seq++;
-//     memset(number, 0, sizeof(number));
-//     sprintf(number, " %d", seq);
-//     strcat(res[0], number);
-//     send(thread_s, res[0], strlen(buf), 0);
-//     memset(buf, 0, sizeof(buf));
-//   }
-//   // pthread_mutex_unlock(&lock);
-// }
